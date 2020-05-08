8b8fd5626f3fae36decd7c5496289541f9759124
“Foster <fosterataylor@yahoo.com>
2020-05-08 11:47:53 -0600

    Send message to phone with create method

--- send-sms.js
+++ send-sms.js
@@ -3,3 +3,9 @@ const authToken = '9cde67bad8e8ba61cff9b72bd2eed57c'
 
 const client = require('twilio')(accountSid, authToken);
 
+client.messages.create({
+    to: '+17132870112',
+    from: '+15013563249',
+    body:'Hey bud its Foster from the past'
+})
+.then((message)=>console.log(message.sid))
