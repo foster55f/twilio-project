ce99f65879bc44bb83b4ad165b4e1f471b418b2c
“Foster <fosterataylor@yahoo.com>
2020-05-08 13:01:42 -0600

    Add more env variables for phone numbers

--- send-sms.js
+++ send-sms.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-
+console.log('Your environment variable TWILIO_ACCOUNT_SID has the value: ', process.env.TWILIO_ACCOUNT_SID);
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
@@ -7,8 +7,8 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 client.messages.create({
-    to: '+17132870112',
-    from: '+15013563249',
+    to: process.env.MY_PHONE,
+    from: process.env.TWILIO_PHONE,
     body:'Hey bud its Foster from the past'
 })
 .then((message)=>console.log(message.sid))
